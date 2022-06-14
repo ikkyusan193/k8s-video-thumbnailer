fe38cc9699e597462cd9513fb7f63e1bf736b32b
ikkyusan193 <ikkiwie07@gmail.com>
2022-06-15 05:05:43 +0700

    fixing dockerfile for frontend

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <h1>YOYOYOYOYOO</h1>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+    }),
+  }
+</script>
---
+++ frontend/vue.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: [
+    'vuetify'
+  ],
+  devServer: {
+    host: "localhost", // frontend hostname or ip
+    disableHostCheck: true,
+    port: 8000, // frontend port
+    https: false, // no ssl
+    proxy: {
+      "/api": {
+        // proxy everything from frontend http://localhost:8080/api/** to backend at http://localhost:8081/api/**
+        // that is why all api path on backend should begin with /api
+        target: "http://localhost:8080",
+      },
+    },
+  },
+})
