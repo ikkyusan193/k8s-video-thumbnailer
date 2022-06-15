3a138b51894a8605e855a7877feb339e6017b691
ikkyusan193 <ikkiwie07@gmail.com>
2022-06-15 15:45:56 +0700

    fix vue config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,18 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ],
+module.exports = {
+  transpileDependencies: ["vuetify"],
   devServer: {
-    host: "localhost", // frontend hostname or ip
-    port: 8000, // frontend port
-    https: false, // no ssl
+    host: "localhost",
+    disableHostCheck: true,
+    port: 8080,
+    https: false,
     proxy: {
       "/api": {
-        // proxy everything from frontend http://localhost:8080/api/** to backend at http://localhost:8081/api/**
-        // that is why all api path on backend should begin with /api
-        target: "http://localhost:8080",
+        target: "http://127.0.0.1:5000",
       },
     },
   },
-})
+};
