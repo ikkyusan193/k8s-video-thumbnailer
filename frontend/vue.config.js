7eff448f9f16040bfaaf0be0b8b46e12d97d290e
ikkyusan193 <ikkiwie07@gmail.com>
2022-06-16 18:39:47 +0700

    done now, forgot to change port!

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,13 +4,13 @@ module.exports = {
   devServer: {
     host: "localhost",
     disableHostCheck: true,
-    //  port: 80, // top = deploy, bot = local
-    port: 8080,
+    port: 80, // top = deploy, bot = local
+    // port: 8080,
     https: false,
     proxy: {
       "/backend": {
-        // target: "http://0.0.0.0.0:5000",
-        target: "http://localhost:80"
+        target: "http://0.0.0.0.0:5000",
+        // target: "http://localhost:80"
       },
     },
   },
